72aa24deb7f0865f1d0f414d9029068ee79d262b
sorabhgwala <sorabhgwala@gmail.com>
2024-11-13 20:46:48 +0530

    productcart edit

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ function App() {
    
     <Nav2bar/>
     <Home/>
+    {/* <Cart/> */}
     {/* <Login/> */}
-    {/* <Product/> */}
-    {/* <Footer/> */}
-    <Cart/>
+    <Product/>
+{/*    
+     <Footer/> */}
       
 
 
